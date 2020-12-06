e2ab9e6d6fef4a9739f8d98eeebd5222f326bb00
Tony Melek <tonymelek.au@gmail.com>
2020-12-06 21:08:30 +1100

    save offline transactions

--- public/index.js
+++ public/index.js
@@ -1,11 +1,32 @@
 let transactions = [];
 let myChart;
+let offlineTransactions = [];
+
+if (localStorage.getItem('offlineTransactions')) {
+  offlineTransactions = JSON.parse(localStorage.getItem('offlineTransactions'))
+  console.log(offlineTransactions, typeof offlineTransactions);
+  $.post('/api/bulk', { data: JSON.stringify(offlineTransactions) })
+    .then(res => {
+      console.log(res);
+      offlineTransactions = [];
+      localStorage.removeItem('offlineTransactions');
+    }).catch(err => {
+      console.log(err);
+    })
+
+}
+
+
+
+
+
 
 fetch("/api/transaction")
   .then(response => {
     return response.json();
   })
   .then(data => {
+    console.log(data);
     // save db data on global variable
     transactions = data;
 
@@ -66,14 +87,14 @@ function populateChart() {
 
   myChart = new Chart(ctx, {
     type: 'line',
-      data: {
-        labels,
-        datasets: [{
-            label: "Total Over Time",
-            fill: true,
-            backgroundColor: "#6666ff",
-            data
-        }]
+    data: {
+      labels,
+      datasets: [{
+        label: "Total Over Time",
+        fill: true,
+        backgroundColor: "#6666ff",
+        data
+      }]
     }
   });
 }
@@ -111,7 +132,7 @@ function sendTransaction(isAdding) {
   populateChart();
   populateTable();
   populateTotal();
-  
+
   // also send to server
   fetch("/api/transaction", {
     method: "POST",
@@ -121,33 +142,36 @@ function sendTransaction(isAdding) {
       "Content-Type": "application/json"
     }
   })
-  .then(response => {    
-    return response.json();
-  })
-  .then(data => {
-    if (data.errors) {
-      errorEl.textContent = "Missing Information";
-    }
-    else {
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      }
+      else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      // saveRecord(transaction);
+      offlineTransactions.unshift(transaction);
+      console.log(offlineTransactions)
+      localStorage.setItem('offlineTransactions', JSON.stringify(offlineTransactions))
       // clear form
       nameEl.value = "";
       amountEl.value = "";
-    }
-  })
-  .catch(err => {
-    // fetch failed, so save in indexed db
-    saveRecord(transaction);
-
-    // clear form
-    nameEl.value = "";
-    amountEl.value = "";
-  });
+    });
 }
 
-document.querySelector("#add-btn").onclick = function() {
+document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
 };
 
-document.querySelector("#sub-btn").onclick = function() {
+document.querySelector("#sub-btn").onclick = function () {
   sendTransaction(false);
 };
+
--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,6 @@
 const staticCacheName = 'site-static';
-const assets = ['/', '/icons/icon-192x192.png', '/icons/icon-512x512.png', '/app.js', '/index.js', '/styles.css', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css']
+const dataCacheName = 'data-cache';
+const assets = ['/', '/icons/icon-192x192.png', '/icons/icon-512x512.png', '/app.js', '/index.js', '/styles.css', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', 'https://code.jquery.com/jquery-3.5.1.min.js']
 //install event
 self.addEventListener('install', (e) => {
     console.log('Service Worker has been installed');
@@ -14,15 +15,46 @@ self.addEventListener('install', (e) => {
 
 //activate event
 self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== staticCacheName && key !== dataCacheName) {
+                        return caches.delete(key)
+                    }
+                })
+            )
+        })
+    )
     console.log('Service worker has been activated');
+    self.clients.claim();
 })
 
 //fetch event
 self.addEventListener('fetch', e => {
-    console.log('fetch event', e.request.url);
-    e.respondWith(
-        caches.match(e.request).then(cacheRes => {
-            return cacheRes || fetch(e.request) //retuen from chache if exists if not try from the server.
-        })
-    )
+    if (e.request.url.includes('/api/')) {
+        e.respondWith(
+            caches.open(dataCacheName).then(cache => {
+                return fetch(e.request)
+                    .then(response => {
+                        if (response.status === 200) {
+                            cache.put(e.request.url, response.clone());
+                        } else {
+                            console.log('server down');
+                        }
+                        return response
+                    })
+                    .catch(err => {
+                        return cache.match(e.request)
+                    })
+            })
+        )
+    }
+    else {
+        e.respondWith(
+            caches.match(e.request).then(cacheRes => {
+                return cacheRes || fetch(e.request) //retuen from chache if exists if not try from the server.
+            })
+        )
+    }
 })
